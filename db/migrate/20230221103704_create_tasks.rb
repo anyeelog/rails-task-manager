53563d046d94e2d48a7c0996ac1cda13cd2931ce
Angie Xu <anyixu98@gmail.com>
2023-02-21 12:34:41 +0100

    Added tasks list and add

---
+++ db/migrate/20230221103704_create_tasks.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Creates the tasks table
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :details
+      t.boolean :completed, default: false
+
+      t.timestamps
+    end
+  end
+end
