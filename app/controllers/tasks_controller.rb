02b43724b757731287bcf9b8a7d1472a6bea8f3e
Angie Xu <anyixu98@gmail.com>
2023-02-21 12:53:21 +0100

    Added update and delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@
 
 # Tasks controller
 class TasksController < ApplicationController
+  # List
   def index
     @tasks = Task.all
   end
@@ -10,6 +11,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  # Create
   def new
     @task = Task.new
   end
@@ -19,6 +21,26 @@ class TasksController < ApplicationController
     @task.save
 
     redirect_to task_path(@task)
+  end
+
+  # Update
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  # Destroy
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # List
   get '/tasks', to: 'tasks#index'
 
+  # Create
   get '/tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
 
+  # Detail
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  # Update
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
